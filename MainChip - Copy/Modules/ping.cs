d009111053a4b62f47ab07874c3bd175ad4ba08f
callum <callum.lickfold@gmail.com>
2018-06-01 20:41:35 +0100

    gg
    chi

---
+++ MainChip - Copy/Modules/ping.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace MainChip.Modules
+{
+
+
+
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello")]
+        public async Task PingAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle("Hello")
+                .WithDescription("How are you?")
+                .WithColor(Color.Gold);
+
+
+            await ReplyAsync("", false, builder.Build());
+
+
+
+
+
+        }
+    }
+
+
+
+    }
+   
+    
+
+   
